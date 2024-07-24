485b8693df083f96699cad075c12f4580ce81109
mgrin <mgrin@luxms.com>
2024-07-24 18:28:55 +0300

    config: add mini-css-extract-plugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,6 +2,7 @@ import { ProgressPlugin, Configuration } from "webpack";
 import 'webpack-dev-server';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 interface Environment {
   [key: string]: string
@@ -21,6 +22,9 @@ export default (env: Environment): Configuration => {
       new ProgressPlugin(),
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, 'public', 'index.html')
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash:8].css'
       })
     ],
     module: {
@@ -34,7 +38,7 @@ export default (env: Environment): Configuration => {
           test: /\.s[ac]ss$/i,
           use: [
             // Creates `style` nodes from JS strings
-            "style-loader",
+            MiniCssExtractPlugin.loader,
             // Translates CSS into CommonJS
             "css-loader",
             // Compiles Sass to CSS
